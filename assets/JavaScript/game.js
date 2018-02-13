2973053a378ea94631d3dfa22b934e9704683b90
Christian Lopes <christianlopes@Christian-Lopess-iMac-2.local>
2018-02-13 13:58:20 -0800

    commented codeadded border to cards

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -9,14 +9,13 @@ $(document).ready(function () {
 
     var chosenHero
     var chosenEnemy
-    var isHeroAlive
-    var isEnemyAlive
     var isHeroChosen
     var isEnemyChosen
     var maxEnemies = 5
     var enemiesLeft = 5
     console.log("There are now " + enemiesLeft + "  enemies left.")
-   
+
+
     // Gets Link for Theme Song
     var audioElement = document.createElement("audio");
     audioElement.setAttribute("src", "./assets/javascript/magneto.mp3");
@@ -33,7 +32,7 @@ $(document).ready(function () {
             var num = Math.floor(12 / charArr.length)
             // var charThing = $("<div class='myChar col-md-" + num + "'value='" + i + "'><img src='" + charArr[i].image + "' style='width:150px;height:200px;border:3px solid gray'/></div>")
             var charThing = $("<div class='card-deck myChar col-sm-" + num +
-                "' value='" + i + "'style='width:14rem;'><div class='card-img-top'><img src='" + charArr[i].image + "' style='width:150px;height:200px;'/></div><div class='card-body'><div class='card-text'>" + charArr[i].name + "<br>" + "Health: " + charArr[i].hp + "<br>" + "Attack: " + charArr[i].attackPower + "<br>" + "Counter Attack: " + charArr[i].counterAttack + "</div></div></div>")
+                "' value='" + i + "'style='width:14rem;'><div class='card-img-top'><img src='" + charArr[i].image + "' style='width:170px;height:250px;border:5px solid gray'/></div><div class='card-body'><div class='card-text'>" + charArr[i].name + "<br>" + "Health: " + charArr[i].hp + "<br>" + "Attack: " + charArr[i].attackPower + "<br>" + "Counter Attack: " + charArr[i].counterAttack + "</div></div></div>")
             $("#characters").append(charThing)
         }
     }
@@ -44,7 +43,6 @@ $(document).ready(function () {
 
         if (isHeroChosen === false) {
             chosenHero = charArr[$(this).attr("value")]
-            console.log(chosenHero)
             $(this).empty()
             $(this).click(false)
             $("#heroImg").append("<img src='" + chosenHero.image + "'/>")
@@ -57,44 +55,42 @@ $(document).ready(function () {
         //selects and grays out the second choice, assigns it as the enemy. 
         else if (isEnemyChosen === false && chosenHero.name != charArr[$(this).attr("value")].name) {
             chosenEnemy = charArr[$(this).attr("value")]
-            console.log(chosenEnemy)
             $(this).empty()
             $(this).click(false)
             $("#enemyImg").append("<img src='" + chosenEnemy.image + "'/>")
             $("#enemyName").append(chosenEnemy.name)
             $("#enemyHp").append("Health: " + chosenEnemy.hp)
-            // $("#enemyAttack").append("Attack Power: " + chosenEnemy.attackPower)
             $("#enemyCtrAttack").append("Counter Attack: " + chosenEnemy.counterAttack)
             isEnemyChosen = true
-            $(document).scrollTop(2000)
+            $(document).scrollTop(800)
             audioElement.play()
-            
         }
 
     })
 
 
-    //Game-play logic
+    //Game-play logic - this is what fires when you click the attack button
     $("#button").on("click", function () {
         debugger
         if (chosenEnemy.hp > 0) {
             $("#heroHp").html("Health: " + (chosenHero.hp -= chosenEnemy.counterAttack))
-            // console.log("The button was clicked, hero health is now: " + chosenHero.hp)
             $("#enemyHp").html("Health: " + (chosenEnemy.hp -= chosenHero.attackPower))
-            // console.log("The button was clicked, hero attack power is now: " + chosenHero.attackPower)
         }
+
+        // Give hero 20 attack points each time they attack opponent.
         $("#heroAttack").html("Attack Power: " + (chosenHero.attackPower += 20))
 
         if (chosenHero.hp < 1 && chosenEnemy.hp < 1) {
             alert("It's a draw! Both players lost this battle.")
             enemiesLeft = maxEnemies
             location.reload()
+           
         }
 
+        //If hero defeats all enemies, alert champion, reload page.
         else if (enemiesLeft === 0) {
             alert("Congratulations, you're the new champion!")
             isEnemyChosen = false
-            // $("#enemyImg").animate({ height: "500px" });
             $("#enemyImg").empty()
             $("#enemyName").empty()
             $("#enemyHp").empty()
@@ -103,17 +99,14 @@ $(document).ready(function () {
             $(document).scrollTop(0)
             location.reload()
         }
+            
 
-
+        //When hero defeats an enemy, remove enemy from battlestage and scroll to top. 
         else if (chosenEnemy.hp < 1) {
             debugger
             enemiesLeft--
-            console.log("There are now " +enemiesLeft+ "  enemies left.")
+            // console.log("There are now " + enemiesLeft + "  enemies left.")
             $("#enemyHp").html("Health: " + (chosenEnemy.hp -= chosenHero.attackPower))
-            // $("#enemyImg").delay( 800 ).fadeOut( "slow" )
-            // $("#enemyName").delay( 800 ).fadeOut( "slow" )
-            // $("#enemyHp").delay( 800 ).fadeOut( "slow" )
-            // $("#enemyCtrAttack").delay( 800 ).fadeOut( "slow" )
             $("#enemyImg").empty()
             $("#enemyName").empty()
             $("#enemyHp").empty()
@@ -122,16 +115,14 @@ $(document).ready(function () {
             alert("You've defeated " + chosenEnemy.name + "! Select your next opponent!")
             isEnemyChosen = false
             $(document).scrollTop(0)
-
         }
 
-
+        //If player gets defeated, alert player, reload game. 
         else if (chosenHero.hp < 1) {
-            // $("#heroImg").fadeOut("slow")
             alert("You've been defeated! Game Over!")
-            // resetGame()
             enemiesLeft = maxEnemies
             location.reload()
+            $(document).scrollTop(0)
         }
 
 
@@ -156,9 +147,6 @@ $(document).ready(function () {
         {
             name: "Juggernaut",
             hp: 120,
-            // attack: function () {
-            //     return Math.floor(Math.random() * 15) + 1
-            // },
             attackPower: 18,
             counterAttack: 10,
             image: "./assets/images/beast.jpeg",
@@ -167,9 +155,6 @@ $(document).ready(function () {
         {
             name: "Captain America",
             hp: 130,
-            // attack: function () {
-            //     return Math.floor(Math.random() * 15) + 1
-            // },
             attackPower: 30,
             counterAttack: 15,
             image: "./assets/images/captainamerica.jpeg",
@@ -178,9 +163,6 @@ $(document).ready(function () {
         {
             name: "Emma Frost",
             hp: 110,
-            // attack: function () {
-            //     return Math.floor(Math.random() * 15) + 1
-            // },
             attackPower: 15,
             counterAttack: 10,
             image: "./assets/images/emmafrost.jpeg",
@@ -190,9 +172,6 @@ $(document).ready(function () {
         {
             name: "Magneto",
             hp: 100,
-            // attack: function () {
-            //     return Math.floor(Math.random() * 15) + 1
-            // },
             attackPower: 40,
             counterAttack: 20,
             image: "./assets/images/magneto.jpeg",
@@ -202,10 +181,6 @@ $(document).ready(function () {
         {
             name: "Night Crawler",
             hp: 130,
-            // attack: function () {
-            //     return Math.floor(Math.random() * 15) + 1
-            // },
-
             attackPower: 15,
             counterAttack: 15,
             image: "./assets/images/nightcrawler.jpeg",
@@ -214,10 +189,6 @@ $(document).ready(function () {
         {
             name: "Spider Man",
             hp: 140,
-            // attack: function () {
-            //     return Math.floor(Math.random() * 15) + 1
-            // },
-
             attackPower: 15,
             counterAttack: 20,
             image: "./assets/images/spiderman.jpg",
